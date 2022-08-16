83de6a6af33bfb36eb489c2d53eacf686f31a599
chiseong kim <dlapdlf1013@gmail.com>
2022-08-16 17:12:57 +0900

    220816

---
+++ OneDayOneAlgorithm/src/main/Algorithm_220816_Backjoon_14425.java
@@ -0,0 +1,57 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Algorithm_220816_Backjoon_14425 {
+
+/*
+ * 
+ * https://www.acmicpc.net/problem/14425
+ * 	
+ */
+	
+	
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		
+		Map<String,Integer> MyMap = new HashMap<String, Integer>();
+		
+		for(int i=0; i<N; i++) {
+			
+			MyMap.put(br.readLine(), i);
+			
+			
+		}
+		
+		int count = 0;
+		
+		for(int i=0; i<M;i++) {
+			
+			if(MyMap.containsKey(br.readLine())){
+				
+				count++;
+				
+			}
+			
+			
+		}
+		
+		
+		System.out.println(count);
+		
+	}
+	
+	
+}
